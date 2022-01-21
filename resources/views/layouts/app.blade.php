33aec17bcd38006c1fb0bbeb581a8d3b2c496f6c
Arturo <art_fortuny@hotmail.com>
2022-01-21 17:00:44 +0100

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Posts</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css"
+    </head>
+    <body>
+
+    <header>
+        <div>
+            <div><h1>My Posts</h1></div>
+        </div>
+    </header>
+    
+    <nav>
+        <ul>
+            <li><a href="/about">About</a></li>
+        </ul>
+    </nav>
+
+    <div class="container">
+        @yield('contents');
+    </div>
+
+    <footer>
+
+    </footer>
+    
+    </body>
+</html>
